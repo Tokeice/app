e124435614dd99728a8ddc67c841351858e74860
nishikawa <b053ffv@gmail.com>
2024-06-21 01:22:30 +0900

    add: 沈黙状態によりアイスブレイク画面の背景の切り替え及びjsonファイルから文字列を取得する処理を作成

--- lib/ice_break.dart
+++ lib/ice_break.dart
@@ -1,8 +1,10 @@
 import 'dart:async';
+import 'dart:convert';
 import 'package:noise_meter/noise_meter.dart';
 import 'package:flutter/material.dart';
 import 'package:permission_handler/permission_handler.dart';
 import 'package:test_nm/result_screen.dart';
+import 'package:flutter/services.dart' show rootBundle;
 
 class IceBreak extends StatefulWidget {
   @override
@@ -10,10 +12,23 @@ class IceBreak extends StatefulWidget {
 }
 
 class _IceBreakState extends State<IceBreak> {
+
   bool _isRecording = false;
   NoiseReading? _latestReading;
   StreamSubscription<NoiseReading>? _noiseSubscription;
   NoiseMeter? noiseMeter;
+
+  int _seconds = 0; // 沈黙判定の秒数カウント用
+  Timer? _timer; // 沈黙判定の秒数カウント用
+  final int _threshold = 60; // 沈黙判定の閾値(dB)
+
+  String theme = "Loading...";
+
+  @override
+  void initState() {
+    super.initState();
+    _loadTheme();
+  }
 
   @override
   void dispose() {
@@ -49,10 +64,50 @@ class _IceBreakState extends State<IceBreak> {
     setState(() => _isRecording = true);
   }
 
+  Future<void> _loadTheme() async {
+    String jsonString = await rootBundle.loadString('assets/topics.json');
+    Map<String, dynamic> jsonData = json.decode(jsonString);
+    setState(() {
+      theme = jsonData['theme'];
+    });
+  }
+
   /// ノイズメーターのサンプリングを停止
   void stop() {
     _noiseSubscription?.cancel();
     setState(() => _isRecording = false);
+  }
+
+  void _startTimer() {
+    if (_timer == null || !_timer!.isActive) {
+      _timer = Timer.periodic(Duration(seconds: 1), (timer) {
+        setState(() {
+          _seconds++;
+        });
+      });
+    }
+  }
+
+  void _stopTimer() {
+    _timer?.cancel();
+    _timer = null;
+    _seconds = 0;
+  }
+
+  Color changeBackground() {
+    if (!_isRecording) { // 録音フラグがfalseの場合
+      return Colors.white;
+    }
+    else if ((_latestReading?.meanDecibel ?? 0 ) > _threshold){ // 音が一定のdBより大きい
+      _startTimer();
+
+      if (_seconds >= 5){ // 一定のdBより大きい状態が5秒以上続く
+        return Colors.red;
+      }
+    }else {
+      _stopTimer();
+    }
+    return Colors.blue;
   }
 
   @override
@@ -64,6 +119,11 @@ class _IceBreakState extends State<IceBreak> {
           Container(
             margin: EdgeInsets.all(25),
             child: Column(children: [
+              Container(
+                child: Text('Thme: $theme',
+                  style: TextStyle(fontSize: 25, color: Colors.blue)),
+                margin: EdgeInsets.only(top: 20),
+              ),
               Container(
                 child: Text(_isRecording ? "Mic: ON" : "Mic: OFF",
                   style: TextStyle(fontSize: 25, color: Colors.blue)),
@@ -89,9 +149,11 @@ class _IceBreakState extends State<IceBreak> {
         ],
       ),
     ),
+    backgroundColor: changeBackground(),
     floatingActionButton: FloatingActionButton(
       child: Text('終了'),
       onPressed: () {
+        stop;
         Navigator.push(
           context,
           MaterialPageRoute(builder: (context) => ResultScreen()),
