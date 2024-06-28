f6b9c822b825e3ffed64b8182345d3571a49ce38
nishikawa <b053ffv@gmail.com>
2024-06-29 00:42:53 +0900

    add: リザルト画面のレイアウトを作成

--- lib/result_screen.dart
+++ lib/result_screen.dart
@@ -1,20 +1,87 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 import 'main.dart';
 
 class ResultScreen extends StatelessWidget {
   @override
-  Widget build(BuildContext context) => Scaffold(
-    body: Center(
-      // 画面遷移のボタン
-      child: ElevatedButton(
-        child: Text('スタート画面に戻る'),
-        onPressed: () {
-          Navigator.push(
-            context,
-            MaterialPageRoute(builder: (context) => TitleScreen()),
-          );
-        },
+  Widget build(BuildContext context) {
+    double screenWidth = MediaQuery.of(context).size.width;
+
+    return Scaffold(
+        body: Container(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          Container(
+            child: Column(children: [
+              Text(
+                '00分00秒',
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                    fontSize: screenWidth * 0.1,
+                    color: Color.fromARGB(0xFF, 0x41, 0x64, 0x98)),
+              ),
+              Text(
+                '盛り上がりました！',
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                    fontSize: screenWidth * 0.1,
+                    color: Color.fromARGB(0xFF, 0x14, 0x25, 0x40)),
+              )
+            ]),
+          ),
+          Container(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                Container(
+                  child: SvgPicture.asset('images/speech_bubble_result.svg',
+                      width: screenWidth * 0.5),
+                ),
+                Container(
+                  child: SvgPicture.asset(
+                    'images/character_with_icecream.svg',
+                    width: screenWidth * 0.5, // 画像の幅を画面幅の50%に指定
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Container(
+            child: Center(
+                child: Stack(
+                  alignment: Alignment.center,
+                  children: [
+                  SizedBox(
+                      child: GestureDetector(
+                        onTap: () {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(builder: (context) => TitleScreen()),
+                          );
+                        },
+                        child: SvgPicture.asset(
+                          'images/button_back_to_title.svg',
+                          width: screenWidth * 0.8,
+                        ),
+                    )
+                  ),
+                  Positioned(
+                    child: Text(
+                      'タイトルにもどる',
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                        fontSize: screenWidth * 0.08
+                      ),
+                    ),
+                  )
+            ])),
+          )
+        ],
       ),
-    ),
-  );
+    ));
+  }
 }
