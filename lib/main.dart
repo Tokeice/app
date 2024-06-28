a319d19661bba704fe1b453853e73701370d87c2
nishikawa <b053ffv@gmail.com>
2024-06-28 21:27:22 +0900

    change: アプリ全体のフォントを変更

--- lib/main.dart
+++ lib/main.dart
@@ -10,6 +10,9 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      theme: ThemeData(
+        fontFamily: 'Zen Maru Gothic' // アプリ全体のフォントの指定
+      ),
       home: TitleScreen(),
     );
   }
