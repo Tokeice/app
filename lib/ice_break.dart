a2f4a67e5f7a9899725e9d624d86bc940a44161f
ksr03 <K_Haruka03@outlook.jp>
2024-07-02 18:56:02 +0900

    やめるボタンの調整

--- lib/ice_break.dart
+++ lib/ice_break.dart
@@ -125,9 +125,12 @@ class _IceBreakState extends State<IceBreak> {
       if (_exciteSeconds >= 5) {
         // 一定のdBより大きい状態が5秒以上続く
         return GestureDetector(
-          child: SvgPicture.asset(
-            'images/button_end_excite.svg',
-            width: screenWidth * 0.4,
+          child: Transform.translate(
+            offset: Offset(screenWidth * 0.1, -screenWidth * 0.1),
+            child: SvgPicture.asset(
+              'images/button_end_excite.svg',
+              width: screenWidth * 0.45,
+            )
           ),
           onTap: () {
             stop();
@@ -140,9 +143,12 @@ class _IceBreakState extends State<IceBreak> {
       }
     }
     return GestureDetector(
-      child: SvgPicture.asset(
-        'images/button_end_silent.svg',
-        width: screenWidth * 0.4,
+      child: Transform.translate(
+        offset: Offset(screenWidth * 0.05, -screenWidth * 0.02),
+        child: SvgPicture.asset(
+          'images/button_end_silent.svg',
+          width: screenWidth * 0.35,
+        )
       ),
       onTap: () {
         stop();
@@ -317,20 +323,20 @@ class _IceBreakState extends State<IceBreak> {
           Align(
             alignment: Alignment.topRight,
             child: changeEndButton(screenWidth)),
-          Container(
-            child: Align(
-              alignment: Alignment.bottomCenter,
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.end,
-                children: [
-                  selfIntroArrow(screenWidth),
-                  selfIntroCharactor(screenWidth),
-                ],
-              ),
-            )
-          ),
+          // Container(
+          //   child: Align(
+          //     alignment: Alignment.bottomCenter,
+          //     child: Column(
+          //       mainAxisAlignment: MainAxisAlignment.end,
+          //       children: [
+          //         selfIntroArrow(screenWidth),
+          //         selfIntroCharactor(screenWidth),
+          //       ],
+          //     ),
+          //   )
+          // ),
           // 以下のコードでキャラクターとトークテーマを表示
-          // directionCharacterSpeechWidget(context, Direction.left, theme),
+          directionCharacterSpeechWidget(context, Direction.left, theme),
         ],
       ),
       backgroundColor: changeBackground(),
