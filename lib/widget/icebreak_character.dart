e0b6d2b5f7ca639ace24e3b85cbeffad9a980f83
nishikawa <b053ffv@gmail.com>
2024-07-07 13:15:07 +0900

    #38 add: 沈黙状態になるとキャラクターの方向を変え、話題を振る機能の追加

--- lib/widget/icebreak_character.dart
+++ lib/widget/icebreak_character.dart
@@ -4,30 +4,22 @@ import 'package:test_nm/widget/self_intro_character.dart';
 import 'package:test_nm/widget/direction_character_speech_widget.dart';
 import 'package:test_nm/type/Direction.dart';
 
-class IcebreakCharacter extends StatelessWidget {
+class IcebreakCharacter extends StatelessWidget{
   const IcebreakCharacter({
     super.key,
     required this.screenWidth,
+    required this.theme,
+    required this.direction,
   });
 
   final double screenWidth;
+  final String theme;
+  final Direction direction;
 
   @override
   Widget build(BuildContext context) {
-    String theme = "Loading...";
-
-    // return Align(
-    //   alignment: Alignment.bottomCenter,
-    //   child: Column(
-    //     mainAxisAlignment: MainAxisAlignment.end,
-    //     children: [
-    //       SelfIntroArrow(screenWidth: screenWidth),
-    //       SelfIntroCharacter(screenWidth: screenWidth),
-    //     ],
-    //   ),
-    // );
     // 以下のコードでキャラクターとトークテーマを表示
     return DirectionCharacterSpeechWidget(
-        direction: Direction.left, text: theme, screenWidth: screenWidth);
+        direction: direction, text: theme, screenWidth: screenWidth);
   }
 }
